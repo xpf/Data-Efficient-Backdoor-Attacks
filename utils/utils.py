c6a9863a5fde3863215447740b25c37b44823c82
Pengfei Xia <1052182877@qq.com>
2022-04-22 21:48:14 +0800

    Add files via upload

--- utils/utils.py
+++ utils/utils.py
@@ -1,15 +1,16 @@
 def get_name(opts, func='search'):
     if func == 'search':
-        name = '{}_{}_{}_{}_{}_{}_{}_{}'.format(
+        name = '{}_{}_{}_{}_{}_{}_{}'.format(
             opts.data_name,
             opts.model_name,
             opts.attack_name,
             opts.trigger,
             opts.target,
             opts.ratio,
-            opts.n_iter,
-            opts.alpha,
+            opts.n_iter
         )
+        if opts.n_iter != 0:
+            name = name + '_{}'.format(opts.alpha)
     elif func == 'transfer':
         name = '{}_{}_{}_{}_{}_{}'.format(
             opts.data_name,
