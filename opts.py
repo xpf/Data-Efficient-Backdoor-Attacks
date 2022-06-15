d6156c7204867232b45e760d3a97bbf80694ee74
Pengfei Xia <1052182877@qq.com>
2022-06-15 13:19:33 +0800

    Add files via upload

--- opts.py
+++ opts.py
@@ -8,18 +8,18 @@ def get_opts():
     parser.add_argument('--data_path', type=str, default='/data/xpf/datasets')
     parser.add_argument('--sample_path', type=str, default='./samples')
 
-    parser.add_argument('--data_name', type=str, default='cifar10')
+    parser.add_argument('--data_name', type=str, default='imagenet10', choices=['cifar10', 'imagenet10'])
     parser.add_argument('--model_name', type=str, default='vgg16')
 
     parser.add_argument('--attack_name', type=str, default='blended')
     parser.add_argument('--trigger', type=str, default='0')
     parser.add_argument('--target', type=int, default=0)
-    parser.add_argument('--ratio', type=float, default=0.01)
+    parser.add_argument('--ratio', type=float, default=0.02)
 
     parser.add_argument('--n_iter', type=int, default=10)
     parser.add_argument('--alpha', type=float, default=0.5)
 
-    parser.add_argument('--samples_idx', type=str, default='cifar10_vgg16_blended_0_0_0.01_10_0.5')
+    parser.add_argument('--samples_idx', type=str, default='cifar10_vgg16_blended_0_0_0.02_10_0.5')
 
     opts = parser.parse_args()
     return opts
